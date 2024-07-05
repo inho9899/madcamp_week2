c0a8a2b3dc01a8a435bec6d2bb4fd885015c6ffb
sinjiwon <shinjw0619@gmail.com>
2024-07-05 16:30:24 +0900

    디버그 워터마크 제거

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,37 @@
+//회원가입 화면
+
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Id'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // 가입 처리 로직 추가
+                Navigator.pop(context);
+              },
+              child: Text('Register'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/auth_service.dart
@@ -0,0 +1,15 @@
+class AuthService {
+  static bool isLoggedIn() {
+    // 로그인 상태를 확인하는 로직을 추가합니다.
+    // 예시로, 로그인을 체크하는 변수를 반환합니다.
+    return false;
+  }
+
+  static void login() {
+    // 로그인 로직을 추가합니다.
+  }
+
+  static void logout() {
+    // 로그아웃 로직을 추가합니다.
+  }
+}

bfe1c2ec498d9a56df021a51df8a791d94f9d07b
sinjiwon <shinjw0619@gmail.com>
2024-07-05 20:47:32 +0900

    7/5

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'utils/auth_service.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() {
+  KakaoSdk.init(nativeAppKey: 'e6f2b28d149730b2e8c55ba532d18474');
   runApp(MyApp());
 }
 
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,11 +1,34 @@
-//로그인 화면
-
 import 'package:flutter/material.dart';
 import 'register_screen.dart';
 import 'home_screen.dart';
 import '../utils/auth_service.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 class LoginScreen extends StatelessWidget {
+  Future<void> _loginWithKakao(BuildContext context) async {
+    try {
+      bool isInstalled = await isKakaoTalkInstalled();
+      if (isInstalled) {
+        try {
+          await UserApi.instance.loginWithKakaoTalk();
+          print('카카오톡으로 로그인 성공');
+        } catch (error) {
+          print('카카오톡으로 로그인 실패: $error');
+        }
+      } else {
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          print('카카오 계정으로 로그인 성공');
+        } catch (error) {
+          print('카카오 계정으로 로그인 실패: $error');
+        }
+      }
+      HomeScreen();
+    } catch (error) {
+      print('카카오 로그인 실패: $error');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +41,7 @@ class LoginScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
-              decoration: InputDecoration(labelText: 'Username'),
+              decoration: InputDecoration(labelText: 'Id'),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Password'),
@@ -44,6 +67,48 @@ class LoginScreen extends StatelessWidget {
               },
               child: Text('Register'),
             ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _loginWithKakao(context),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.yellow, // 카카오톡 버튼 색상
+              ),
+              child: Text(
+                'Login with Kakao',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UserInfoPage extends StatelessWidget {
+  final User user;
+
+  UserInfoPage({required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Info'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('User ID: ${user.id}', style: TextStyle(fontSize: 18)),
+            SizedBox(height: 8),
+            Text('Nickname: ${user.kakaoAccount?.profile?.nickname}', style: TextStyle(fontSize: 18)),
+            SizedBox(height: 8),
+            Text('Email: ${user.kakaoAccount?.email}', style: TextStyle(fontSize: 18)),
+            SizedBox(height: 8),
+            if (user.kakaoAccount?.profile?.thumbnailImageUrl != null)
+              Image.network(user.kakaoAccount!.profile!.thumbnailImageUrl!)
           ],
         ),
       ),

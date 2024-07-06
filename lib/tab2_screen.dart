83ee5af8e71a0d4849153dacdbe23a90225f1a34
sinjiwon <shinjw0619@gmail.com>
2024-07-06 18:25:08 +0900

    7/5

--- lib/tab2_screen.dart
+++ lib/tab2_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class Tab2Screen extends StatelessWidget {
-
   // 예시 데이터 (DB에서 가져온다고 가정)
   final String albumImage = 'assets/images/album_cover.jpeg'; // 앨범 커버 이미지 경로
   final String songTitle = 'DB에서 제목 가져오기'; // 노래 제목
@@ -15,6 +15,8 @@ class Tab2Screen extends StatelessWidget {
     // 플레이리스트 탭으로 이동하는 로직
   }
 
+  // 리뷰 작성
+  /*
   void _writeReview(BuildContext context) {
     showDialog(
       context: context,
@@ -40,6 +42,69 @@ class Tab2Screen extends StatelessWidget {
               },
             ),
           ],
+        );
+      },
+    );
+  }
+*/
+  void _writeReview(BuildContext context) {
+    double _rating = 0.0;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('리뷰 작성'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('별점을 선택해주세요'),
+                  SizedBox(height: 8),
+                  RatingBar.builder(
+                    initialRating: 0,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    onRatingUpdate: (rating) {
+                      setState(() {
+                        _rating = rating;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    decoration: InputDecoration(hintText: "리뷰를 입력하세요"),
+                    maxLines: 5,
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  child: Text('취소'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: Text('저장'),
+                  onPressed: () {
+                    // 리뷰와 별점을 저장하는 로직
+                    // 예: DB로 데이터를 넘기는 코드 추가
+                    print('별점: $_rating'); // 추후 DB로 넘기기 위한 로그
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
         );
       },
     );
